7ec702ef68b9923d76f2070e501dde16289b9860
TING-YU WU <125854700+wuray890521@users.noreply.github.com>
2024-05-08 02:57:08 +0800

    Add files via upload

--- fortran/fermi.f90
+++ fortran/fermi.f90
@@ -5,23 +5,24 @@ PROGRAM fermi
     INTEGER :: iounit
     ! Boltzmann constant by wiki in eV/K
     REAL, parameter :: kb = 8.617343E-5 
-    PRINT *, "The value of Boltzmann constant is: ", kb
     PRINT *, 'Input temp.:'
     READ (*,*) t
-    PRINT *, 'Input chemical potential:'
+    PRINT *, 'Input chemical potential:'    
     READ (*,*) mu
     PRINT *, 'Input fermi_energy:'
     READ (*,*) ef
-
     ! Open the file for writing
-    OPEN(UNIT=iounit, FILE='fermi_output.txt', STATUS='REPLACE', ACTION='WRITE')
-
+    OPEN(UNIT=iounit, FILE='fermi_output.dat', STATUS='REPLACE', ACTION='WRITE')
+    IF (t < 10) THEN
+        t = 10
+    ELSE 
+        t = t
+    END IF
     ! Loop to calculate and write the values of f to the file
-    do i = 1, 100000
-        f = 1/(EXP(((DBLE(i)/10000)-ef-mu)/(kb*t)) + 1)
+    do i = 1, 1000
+        f = 1/(EXP(((DBLE(i)/100)-ef-mu)/(kb*t)) + 1)
         WRITE (iounit,*) DBLE(i)/10000, f
     end do
-
     ! Close the file
     CLOSE(iounit)
 END PROGRAM fermi
